7fa91c6e674b809c3a4f5175ab4ef22ad0df07f6
Syrus Akbary <dimension.net@gmail.com>
2011-06-09 12:47:49 +0200

    Changed yuisettings

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,13 @@
-from fabric.api import local
+from fabric.api import local,run, hosts, env, cd
 
 def commit():
     local("git add . && git commit")
     
 def push():
     local("git push origin master")
+
+@hosts('syrus@syrusakbary.com')
+def update(install_requirements=True):
+    with cd('/var/www/vhosts/syrusakbary.com'):
+        run('ls')
+        if install_requirements: run('pip install -r requirements.txt')
