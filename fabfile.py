a52f4c7b96e0d819c68caf48db2b1b8c8fb14768
Syrus Akbary <dimension.net@gmail.com>
2011-06-09 14:21:43 +0200

    Modificados metodos de fab

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,28 @@
-from fabric.api import local,run, hosts, env, cd
+from __future__ import with_statement
+from fabric.api import *
+from contextlib import contextmanager as _contextmanager
+
+env.activate = 'source virtualenv/bin/activate'
 
 def commit():
     local("git add . && git commit")
     
 def push():
     local("git push origin master")
+            
 
+@_contextmanager
+def virtualenv():
+    with prefix(env.activate):
+        yield
+            
 @hosts('syrus@syrusakbary.com')
-def update(install_requirements=True):
+def update(install_requirements=False,generate_media=True):
+    commit()
+    push()
     with cd('/var/www/vhosts/syrusakbary.com'):
-        run('ls')
-        if install_requirements: run('pip install -r requirements.txt')
+        run('git pull')
+        with virtualenv():
+            if install_requirements: run('pip install -r requirements.txt')
+            if generate_media: run('python manage.py generatemedia')
+        #if install_requirements: run('pip install -r requirements.txt')
