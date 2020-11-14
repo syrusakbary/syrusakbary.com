9ca0d3e73d4f982ad206c66cbd7b2cb493942966
Syrus <me@syrusakbary.com>
2020-11-14 01:11:22 -0800

    Enable Google Analytics

--- pages/_page.js
+++ pages/_page.js
@@ -71,7 +71,7 @@ export default function Page({ children, blackHeader }) {
       <div id="pages">
         <div className="page">{children}</div>
       </div>
-      {/* <script>{`
+      <script>{`
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -80,7 +80,6 @@ export default function Page({ children, blackHeader }) {
         ga('create', 'UA-12613282-2', 'auto');
         ga('send', 'pageview', window.location.pathname);
         `}</script>
-      <script>{`window._sharedData = {'view': 'base'};`}</script> */}
 
       <style jsx>{``}</style>
 
