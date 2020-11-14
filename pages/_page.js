2b1d00bec474bd67a8743f60a7ac6f21c6f191e1
Syrus <me@syrusakbary.com>
2020-11-14 08:54:08 -0800

    Fixed Google Analytics

--- pages/_page.js
+++ pages/_page.js
@@ -19,6 +19,14 @@ export default function Page({ children, blackHeader }) {
         <meta name="twitter:site" content="@syrusakbary" />
         <meta name="twitter:title" content="Syrus Akbary" />
         <meta name="twitter:image" content="/images/logo.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YTJ0Q0LJBX"></script>
+        <script dangerouslySetInnerHTML={{__html:`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YTJ0Q0LJBX');
+        `}} />
       </Head>
       <header key="header" id="header" className={blackHeader ? "header--black" : ""}>
         <a id="nav"></a>
@@ -71,16 +79,6 @@ export default function Page({ children, blackHeader }) {
       <div id="pages">
         <div className="page">{children}</div>
       </div>
-      <script>{`
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        
-        ga('create', 'UA-12613282-2', 'auto');
-        ga('send', 'pageview', window.location.pathname);
-        `}</script>
-
       <style jsx>{``}</style>
 
       <style jsx global>{`
